d8b43311621b63c192bca9955bfb8eba9924d1b4
CARLOS GABRIEL SANTOS DE MELO <carlosgabrielsns@gmail.com>
2019-08-25 16:02:09 -0300

    Função para ler o labirinto

--- URI-1621.c
+++ URI-1621.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MALLOC(t,n) (t*)malloc(sizeof(t)*n)
+
+void readEOL(){
+    char c;
+    c = getchar();
+    while(c != '\n')
+        c = getchar();
+}
 
 int main(int argc, char** argv){
+    int N,M;
+    scanf("%d%d",&N,&M);    
+    readEOL();
+    while(N != 0 && M != 0){
+        char **labirinto = MALLOC(char*,N);
+        for(int i=0;i<N;i++)
+            labirinto[i] = MALLOC(char,M);
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                labirinto[i][j]=getchar();
+            }
+           readEOL();
+        }
+        
+        scanf("%d%d",&N,&M);
+        if(N != 0 && M != 0)    
+            readEOL();
+    }
     return 0;
 }
